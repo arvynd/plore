f365788975e1d76d9fd59060510d9a11a5d6168f
Aravind <aravindpramod@outlook.com>
2026-04-26 22:59:03 +0530

    TS extension with sidecar spawn and Arrow IPC byte logging

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,38 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import * as cp from 'child_process';
+import * as path from 'path';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+	const channel = vscode.window.createOutputChannel('Plore');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "plore" is now active!');
-
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
 	const disposable = vscode.commands.registerCommand('plore.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from Plore!');
+		const filePath = path.join(context.extensionPath, 'samples', 'test.csv');
+		runSidecar(filePath, channel);
 	});
 
 	context.subscriptions.push(disposable);
 }
 
-// This method is called when your extension is deactivated
+function runSidecar(filePath: string, channel: vscode.OutputChannel) {
+	const sidecarPath = path.join(__dirname, '..', 'sidecar', 'sidecar.py');
+	const proc = cp.spawn('/home/aravind/Projects/plore/.venv/bin/python', [sidecarPath, filePath]);
+
+	const chunks: Buffer[] = [];
+
+	proc.stdout.on('data', (chunk: Buffer) => {
+		chunks.push(chunk);
+	});
+
+	proc.stdout.on('close', () => {
+		const total = Buffer.concat(chunks);
+		channel.appendLine(`Received ${total.length} bytes from sidecar`);
+		channel.show();
+	});
+
+	proc.stderr.on('data', (data: Buffer) => {
+		channel.appendLine(`[sidecar error] ${data.toString()}`);
+		channel.show();
+	});
+}
+
 export function deactivate() {}
